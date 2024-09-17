c09376aadbe02da35a44e4183ab1930181c5c70e
felipeolmos <felipeolmosf@gmail.com>
2024-09-17 02:01:24 -0300

    Configuración de la base para GitHub Pages en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mi-proyecto-pizzeria/', // Ajusta esto con el nombre exacto de tu repositorio
 })
